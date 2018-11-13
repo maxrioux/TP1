5047145383ca4ffa6ffb5014485f839f24ffb6ba
maxrioux <maxrioux@live.ca>
2018-11-13 10:28:49 -0500

    first commit

---
+++ Tp1/Tp1/Page_maître.Master.designer.cs
@@ -0,0 +1,69 @@
+﻿//------------------------------------------------------------------------------
+// <généré automatiquement>
+//     Ce code a été généré par un outil.
+//
+//     Les modifications apportées à ce fichier peuvent provoquer un comportement incorrect et seront perdues si
+//     le code est régénéré.
+// </généré automatiquement>
+//------------------------------------------------------------------------------
+
+namespace Tp1 {
+    
+    
+    public partial class Page_maître {
+        
+        /// <summary>
+        /// Contrôle form1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Contrôle logo.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image logo;
+        
+        /// <summary>
+        /// Contrôle Image1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
+        
+        /// <summary>
+        /// Contrôle menu.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu menu;
+        
+        /// <summary>
+        /// Contrôle cph_contenu.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder cph_contenu;
+        
+        /// <summary>
+        /// Contrôle logo_cegep1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image logo_cegep1;
+    }
+}
---
+++ Tp1/Tp1/Pages/Slan.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Page_maître.Master" AutoEventWireup="true" CodeBehind="Slan.aspx.cs" Inherits="Tp1.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="cph_contenu" runat="server">
+    <div style="color:white; width: 80%; ">
+Le SLAN est un évènement annuel organisé par la cohorte des troisièmes années de la technique en Informatique. Des centaines de joueurs passionnés de jeux vidéo se retrouvent dans le gymnase du Cégep de Sept-Îles pour partager leur passion pendant 24 heures. Plusieurs tournois de jeux vidéo sont organisés offrant des prix succulents aux gagnants ainsi que des prix de présence au cours de la soirée.
+Plus d’informations sur le site officiel du SLAN Révolution. BROUILLON TABARNAK DE CALISS
+    </div>
+        <asp:Image class="img"
+                ID="img_slan" 
+                ImageUrl="~/img/slan.jpg" 
+                runat="server"
+                width="80%" />
+</asp:Content>
